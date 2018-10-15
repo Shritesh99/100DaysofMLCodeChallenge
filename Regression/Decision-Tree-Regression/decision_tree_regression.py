89756f61dac0659300aabc3f2ab856b55d1ba37e
Shritesh Jamulkar <shritesh.sj@gmail.com>
2018-10-15 05:50:49 +0530

    Added Decision Tree Regression

--- Regression/Decision-Tree-Regression/decision_tree_regression.py
+++ Regression/Decision-Tree-Regression/decision_tree_regression.py
@@ -1,4 +1,4 @@
-# SVR
+# Decision Tree Regression
 
 # Importing the libraries
 import numpy as np
@@ -15,36 +15,27 @@ y = dataset.iloc[:, 2].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
 
 # Feature Scaling
-from sklearn.preprocessing import StandardScaler
+"""from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
-X = sc_X.fit_transform(X)
-y = sc_y.fit_transform(y)
+y_train = sc_y.fit_transform(y_train)"""
 
-# Fitting SVR to the dataset
-from sklearn.svm import SVR
-regressor = SVR(kernel = 'rbf')
+# Fitting Decision Tree Regression to the dataset
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state = 0)
 regressor.fit(X, y)
 
 # Predicting a new result
-y_pred = sc_X.inverse_transform(np.array([[6.5]]))
-y_pred = sc_y.inverse_transform(regressor.predict(y_pred))
+y_pred = regressor.predict(np.array([[6.5]])
 
-
-# Visualising the SVR results
-plt.scatter(X, y, color = 'red')
-plt.plot(X, regressor.predict(X), color = 'blue')
-plt.title('Truth or Bluff (SVR)')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
-plt.show()
-
-# Visualising the SVR results (for higher resolution and smoother curve)
-X_grid = np.arange(min(X), max(X), 0.01) # choice of 0.01 instead of 0.1 step because the data is feature scaled
+# Visualising the Decision Tree Regression results (higher resolution)
+X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (SVR)')
+plt.title('Truth or Bluff (Decision Tree Regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
