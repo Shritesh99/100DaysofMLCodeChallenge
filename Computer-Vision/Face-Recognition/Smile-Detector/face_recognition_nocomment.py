c67e4df2e27fc4f0cdd4df4e325c483b957e58fb
Shritesh Jamulkar <Shritesh.sj@gmail.com>
2018-12-15 02:27:03 +0530

    Added smile detector

--- Computer-Vision/Face-Recognition/Smile-Detector/face_recognition_nocomment.py
+++ Computer-Vision/Face-Recognition/Smile-Detector/face_recognition_nocomment.py
@@ -5,7 +5,7 @@ import cv2
 
 # Loading the cascades
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 
 # Defining a function that will do the detections
 def detect(gray, frame):
@@ -14,9 +14,9 @@ def detect(gray, frame):
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
-        for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+        smile = smile_cascade.detectMultiScale(roi_gray, 1.7, 22)
+        for (sx, sy, sw, sh) in smile:
+            cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0, 255, 0), 2)
     return frame
 
 # Doing some Face Recognition with the webcam
