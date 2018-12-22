c14fa1efaa621e204a5970be0aab2e73ae0c76d2
Shritesh Jamulkar <Shritesh.sj@gmail.com>
2018-12-22 19:10:00 +0530

    Added Generator For GANs

---
+++ Computer-Vision/GANs/dcgan_commented.py
@@ -0,0 +1,65 @@
+# Deep Convolutional GANs
+
+# Importing the libraries
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+from torch.autograd import Variable
+
+# Setting some hyperparameters
+batchSize = 64 # We set the size of the batch.
+imageSize = 64 # We set the size of the generated images (64x64).
+
+# Creating the transformations
+transform = transforms.Compose([transforms.Scale(imageSize), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),]) # We create a list of transformations (scaling, tensor conversion, normalization) to apply to the input images.
+
+# Loading the dataset
+dataset = dset.CIFAR10(root = './data', download = True, transform = transform) # We download the training set in the ./data folder and we apply the previous transformations on each image.
+dataloader = torch.utils.data.DataLoader(dataset, batch_size = batchSize, shuffle = True, num_workers = 2) # We use dataLoader to get the images of the training set batch by batch.
+
+# Defining the weights_init function that takes as input a neural network m and that will initialize all its weights.
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+
+# Defining the generator
+
+class G(nn.Module): # We introduce a class to define the generator.
+
+    def __init__(self): # We introduce the __init__() function that will define the architecture of the generator.
+        super(G, self).__init__() # We inherit from the nn.Module tools.
+        self.main = nn.Sequential( # We create a meta module of a neural network that will contain a sequence of modules (convolutions, full connections, etc.).
+            nn.ConvTranspose2d(100, 512, 4, 1, 0, bias = False), # We start with an inversed convolution.
+            nn.BatchNorm2d(512), # We normalize all the features along the dimension of the batch.
+            nn.ReLU(True), # We apply a ReLU rectification to break the linearity.
+            nn.ConvTranspose2d(512, 256, 4, 2, 1, bias = False), # We add another inversed convolution.
+            nn.BatchNorm2d(256), # We normalize again.
+            nn.ReLU(True), # We apply another ReLU.
+            nn.ConvTranspose2d(256, 128, 4, 2, 1, bias = False), # We add another inversed convolution.
+            nn.BatchNorm2d(128), # We normalize again.
+            nn.ReLU(True), # We apply another ReLU.
+            nn.ConvTranspose2d(128, 64, 4, 2, 1, bias = False), # We add another inversed convolution.
+            nn.BatchNorm2d(64), # We normalize again.
+            nn.ReLU(True), # We apply another ReLU.
+            nn.ConvTranspose2d(64, 3, 4, 2, 1, bias = False), # We add another inversed convolution.
+            nn.Tanh() # We apply a Tanh rectification to break the linearity and stay between -1 and +1.
+        )
+
+    def forward(self, input): # We define the forward function that takes as argument an input that will be fed to the neural network, and that will return the output containing the generated images.
+        output = self.main(input) # We forward propagate the signal through the whole neural network of the generator defined by self.main.
+        return output # We return the output containing the generated images.
+
+# Creating the generator
+netG = G() # We create the generator object.
+netG.apply(weights_init) # We initialize all the weights of its neural network.
+
---
+++ Computer-Vision/GANs/dcgan_nocomment.py
@@ -0,0 +1,65 @@
+# Deep Convolutional GANs
+
+# Importing the libraries
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+from torch.autograd import Variable
+
+# Setting some hyperparameters
+batchSize = 64 # We set the size of the batch.
+imageSize = 64 # We set the size of the generated images (64x64).
+
+# Creating the transformations
+transform = transforms.Compose([transforms.Scale(imageSize), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),]) # We create a list of transformations (scaling, tensor conversion, normalization) to apply to the input images.
+
+# Loading the dataset
+dataset = dset.CIFAR10(root = './data', download = True, transform = transform) # We download the training set in the ./data folder and we apply the previous transformations on each image.
+dataloader = torch.utils.data.DataLoader(dataset, batch_size = batchSize, shuffle = True, num_workers = 2) # We use dataLoader to get the images of the training set batch by batch.
+
+# Defining the weights_init function that takes as input a neural network m and that will initialize all its weights.
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+
+# Defining the generator
+
+class G(nn.Module):
+
+    def __init__(self):
+        super(G, self).__init__()
+        self.main = nn.Sequential(
+            nn.ConvTranspose2d(100, 512, 4, 1, 0, bias = False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(512, 256, 4, 2, 1, bias = False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(256, 128, 4, 2, 1, bias = False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(128, 64, 4, 2, 1, bias = False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 3, 4, 2, 1, bias = False),
+            nn.Tanh()
+        )
+
+    def forward(self, input):
+        output = self.main(input)
+        return output
+
+# Creating the generator
+netG = G()
+netG.apply(weights_init)
+
